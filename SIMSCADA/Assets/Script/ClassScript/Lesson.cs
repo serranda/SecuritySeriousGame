658250d255bccd98472334d2cb26b14ee3e138c4
serranda <marchettiandreab474@gmail.com>
2019-09-13 21:12:33 +0200

    Updated chapter 6 thesis

--- SIMSCADA/Assets/Script/ClassScript/Lesson.cs
+++ SIMSCADA/Assets/Script/ClassScript/Lesson.cs
@@ -1,24 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Lesson
 {
     public string id;
     public string textPath;
-    public string textBody
-    {
-        get
-        {
-            return string.IsNullOrEmpty(textPath)
-                ? string.Empty
-                : Resources.Load<TextAsset>("LessonsBody/" + textPath + "_IT").text;
-        }
 
-    }
+    public string textBody =>
+        string.IsNullOrEmpty(textPath)
+            ? string.Empty
+            : Resources.Load<TextAsset>("LessonsBody/" + textPath + "_IT").text;
 
     public override string ToString()
     {
-        return string.Format("Id: {0}, TextPath: {1}", id, textPath);
+        return $"Id: {id}, TextPath: {textPath}";
     }
 }
